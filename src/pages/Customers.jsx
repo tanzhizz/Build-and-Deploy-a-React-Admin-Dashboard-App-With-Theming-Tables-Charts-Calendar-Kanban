bc825636fb100c130d948437d83f7fae97c50d70
tanzhizz <tanzhizhen1982@gmail.com>
2022-10-01 22:51:29 +0800

    Build Customers Page.

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Selection, Inject, Edit, Toolbar, Sort, Filter } from '@syncfusion/ej2-react-grids';
+
+import { customersData, customersGrid } from '../data/dummy';
+import { Header } from '../components';
 
 const Customers = () => {
   return (
-    <div>Customers</div>
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+    <Header category="Page" title="Customers" />
+    <GridComponent
+      dataSource={customersData}
+      allowPaging
+      allowSorting
+      toolbar={['Delete']}
+      width="auto"
+      editSettings={{ allowDeleting: true, allowEditing: true }}
+      // allowExcelExport
+      // allowPdfExport
+      // contextMenuItems={contextMenuItems}
+      // editSettings={editing}
+    >
+      <ColumnsDirective>
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        {customersGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
+      </ColumnsDirective>
+      <Inject services={[Page, Selection, Toolbar, Edit, Sort, Filter]} />
+    </GridComponent>
+  </div>
   )
 }
 
