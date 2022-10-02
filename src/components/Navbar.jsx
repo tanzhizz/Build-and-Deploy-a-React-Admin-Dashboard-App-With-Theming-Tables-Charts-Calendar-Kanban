e9fee0ea2d831f2b23ccb336b1ed490f38e94519
tanzhizz <tanzhizhen1982@gmail.com>
2022-10-02 17:01:36 +0800

    Theme Fixes.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const NavButton = ({title, customFunc, icon, color, dotColor}) => (
 
 
 const Navbar = () => {
-  const { activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick, screenSize, setScreenSize } = useStateContext();
+  const { activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick, screenSize, setScreenSize, currentColor } = useStateContext();
 
   useEffect(() => {
     const handleResize = () => setScreenSize(window.innerWidth);
@@ -46,7 +46,7 @@ const Navbar = () => {
       <NavButton 
       title="Menu" 
       customFunc={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)} 
-      color="blue" 
+      color={currentColor} 
       icon={<AiOutlineMenu />} 
       />
 
@@ -54,7 +54,7 @@ const Navbar = () => {
       <NavButton 
       title="Cart" 
       customFunc={() => handleClick("cart")} 
-      color="blue" 
+      color={currentColor} 
       icon={<FiShoppingCart />} 
       />
 
@@ -62,7 +62,7 @@ const Navbar = () => {
       title="Chat" 
       dotColor="#03C9D7"
       customFunc={() => handleClick("chat")} 
-      color="blue" 
+      color={currentColor} 
       icon={<BsChatLeft />} 
       />
 
@@ -70,7 +70,7 @@ const Navbar = () => {
       title="Notifications" 
       dotColor="#03C9D7"
       customFunc={() => handleClick("notification")} 
-      color="blue" 
+      color={currentColor} 
       icon={<RiNotification3Line />} 
       />
 
--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -8,6 +8,8 @@ import {SparklineAreaData, earningData, ecomPieChartData} from "../data/dummy";
 
 
 const Ecommerce = () => {
+  const { currentColor, currentMode } = useStateContext();
+
   return (
     <div className='mt-12'>
       <div className='flex flex-wrap lg:flex-nowrap justify-center'>
@@ -26,7 +28,7 @@ const Ecommerce = () => {
             <div className='mt-6'>
                 <Button 
                 color="white"
-                bgColor="blue"
+                bgColor={currentColor}
                 text="Download"
                 borderRadius="10px"
                 size="md"
@@ -117,18 +119,18 @@ const Ecommerce = () => {
                   </div>
                   <div className="mt-5">
                   <SparkLine 
-                  currentColor="blue"
+                  currentColor={currentColor}
                   id="line-sparkLine" 
                   type="Line" 
                   height="80px" 
                   width="250px" 
                   data={SparklineAreaData} 
-                  color="blue" />
+                  color={currentColor} />
                   </div>
                   <div className='mt-10'>
                     <Button
                     color="white"
-                    bgColor="blue"
+                    bgColor={currentColor}
                     text="Download Report"
                     borderRadius="10px"
                     /> 
